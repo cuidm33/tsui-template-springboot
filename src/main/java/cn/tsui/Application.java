330bf52514d2f5c6d3536b1b73219bce98c6c370
Ming <cuidm33@163.com>
2020-01-02 15:36:10 +0800

    1.0

---
+++ src/main/java/cn/tsui/Application.java
@@ -0,0 +1,12 @@
+package cn.tsui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+        System.out.println("1.0");
+    }
+}
